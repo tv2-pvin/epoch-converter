5d33e84d6aba294295a5b7f2ecee6f174cf9a0be
tv2-pvin <pvin@tv2.dk>
2022-11-16 10:38:07 +0100

    formatting

--- src/test/java/com/example/EpochConverterCommandTest.java
+++ src/test/java/com/example/EpochConverterCommandTest.java
@@ -1,30 +1,29 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import io.micronaut.configuration.picocli.PicocliRunner;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.env.Environment;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @Disabled
 public class EpochConverterCommandTest {
 
-    @Test
-    public void testWithCommandLineOption() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(baos));
+  @Test
+  public void testWithCommandLineOption() {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(baos));
 
-        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
-            String[] args = new String[] { "-v" };
-            PicocliRunner.run(EpochConverterCommand.class, ctx, args);
+    try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+      String[] args = new String[]{"-v"};
+      PicocliRunner.run(EpochConverterCommand.class, ctx, args);
 
-            // epoch-converter
-            assertTrue(baos.toString().contains("Hi!"));
-        }
+      // epoch-converter
+      assertTrue(baos.toString().contains("Hi!"));
     }
+  }
 }
